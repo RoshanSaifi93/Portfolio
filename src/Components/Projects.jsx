5a9bb4c7a893ea828a8a0de7861db017f7106bdb
RoshanSaifi93 <saifiroshan88@gmail.com>
2026-01-25 19:20:37 +0530

    changes on projects

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -6,35 +6,35 @@ const projectData = [
     description:
       "Modern and responsive portfolio website built with React and Vite to showcase my frontend development skills and projects.",
     live: "#",
-    github: "https://github.com/RoshanSaifi93/Portfolio.git",
+    github: "https://github.com/RoshanSaifi93/Portfolio",
   },
   {
     title: "Todo App",
     description:
       "A simple and interactive Todo application built using React, allowing users to add, manage, and delete tasks with a clean UI and React component architecture.",
     live: "#",
-    github: "https://github.com/RoshanSaifi93/ToDo-React-App.git",
+    github: "https://github.com/RoshanSaifi93/ToDo-React-App",
   },
   {
     title: "Weather App",
     description:
       "A responsive Weather App built with React that fetches real-time weather data using OpenWeather API. Features include city search, current location weather, loading states, error handling, and mobile-friendly UI.",
     live: "#",
-    github: "https://github.com/RoshanSaifi93/Weather-App.git",
+    github: "https://github.com/RoshanSaifi93/Weather-App",
   },
   {
     title: "Expense Tracker",
     description:
       "A simple and responsive Expense Tracker application built with React and Bootstrap. Users can add and delete expenses, track total spending, and persist data using LocalStorage.",
     live: "#",
-    github: "https://github.com/RoshanSaifi93/Expense-Tracker.git",
+    github: "https://github.com/RoshanSaifi93/Expense-Tracker",
   },
   {
     title: "E-commerce UI",
     description:
       "A mini Amazon homepage clone built using HTML and CSS, featuring a responsive layout, navigation bar, product sections, and footer, designed to showcase frontend UI skills.",
     live: "#",
-    github: "https://github.com/RoshanSaifi93/Amazon-UI-Clone.git",
+    github: "https://github.com/RoshanSaifi93/Amazon-UI-Clone",
   },
 ];
 
@@ -54,10 +54,19 @@ const Projects = () => {
             <p>{project.description}</p>
 
             <div className="project-links">
-              <a href={project.live} target="_blank">
+              <a
+                href={project.live}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 Live
               </a>
-              <a href={project.github} target="_blank">
+
+              <a
+                href={project.github}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 GitHub
               </a>
             </div>
